ede255261793c38eeb34164a43c3aa11b7e852cc
Stephen Bradbury <bradbury.ste@googlemail.com>
2025-07-11 13:46:41 +0100

    initial commit for school/academy governance

---
+++ DfE.FindInformationAcademiesTrusts/Pages/Schools/Governance/Historic.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using DfE.FindInformationAcademiesTrusts.Pages.Shared;
+using DfE.FindInformationAcademiesTrusts.Services.DataSource;
+using DfE.FindInformationAcademiesTrusts.Services.School;
+using DfE.FindInformationAcademiesTrusts.Services.Trust;
+
+namespace DfE.FindInformationAcademiesTrusts.Pages.Schools.Governance;
+
+public class HistoricModel(
+    ISchoolService schoolService,
+    ITrustService trustService,
+    IDataSourceService dataSourceService,
+    ISchoolNavMenu schoolNavMenu)
+    : GovernanceAreaModel(schoolService, trustService, dataSourceService, schoolNavMenu)
+{
+    public const string SubPageName = "Historic govenors";
+
+    public override PageMetadata PageMetadata => base.PageMetadata with { SubPageName = SubPageName };
+
+    public static string NavTitle(int number)
+    {
+        return SubPageName + $"({number})";
+    }
+}
---
+++ DfE.FindInformationAcademiesTrusts/Services/School/SchoolGovernanceServiceModel.cs.cs
@@ -0,0 +1,5 @@
+﻿using DfE.FindInformationAcademiesTrusts.Data.Repositories;
+
+namespace DfE.FindInformationAcademiesTrusts.Services.School;
+
+public record SchoolGovernanceServiceModel(Governor[] Current, Governor[] Historic);
