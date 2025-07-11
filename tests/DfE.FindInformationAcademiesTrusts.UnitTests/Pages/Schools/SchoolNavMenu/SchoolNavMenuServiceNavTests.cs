cb3f451f6d45d72423b7ecbe558f6b4b5a2a5720
Stephen Bradbury <bradbury.ste@googlemail.com>
2025-07-11 14:01:34 +0100

    fixes for existing tests

--- tests/DfE.FindInformationAcademiesTrusts.UnitTests/Pages/Schools/SchoolNavMenu/SchoolNavMenuServiceNavTests.cs
+++ tests/DfE.FindInformationAcademiesTrusts.UnitTests/Pages/Schools/SchoolNavMenu/SchoolNavMenuServiceNavTests.cs
@@ -73,6 +73,12 @@ public class SchoolNavMenuServiceNavTests : SchoolNavMenuTestsBase
                 l.LinkDisplayText.Should().Be("Contacts");
                 l.AspPage.Should().Be("/Schools/Contacts/InSchool");
                 l.TestId.Should().Be("contacts-nav");
+            },
+            l =>
+            {
+                l.LinkDisplayText.Should().Be("Governance");
+                l.AspPage.Should().Be("/Schools/Governance/Current");
+                l.TestId.Should().Be("governance-nav");
             }
         );
     }
@@ -98,6 +104,12 @@ public class SchoolNavMenuServiceNavTests : SchoolNavMenuTestsBase
                 l.LinkDisplayText.Should().Be("Contacts");
                 l.AspPage.Should().Be("/Schools/Contacts/InDfe");
                 l.TestId.Should().Be("contacts-nav");
+            },
+            l =>
+            {
+                l.LinkDisplayText.Should().Be("Governance");
+                l.AspPage.Should().Be("/Schools/Governance/Current");
+                l.TestId.Should().Be("governance-nav");
             }
         );
     }
