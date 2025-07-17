b5dba72a2625270109f5fe58a9767df37bfb2e44
Stephen Bradbury <bradbury.ste@googlemail.com>
2025-07-17 09:40:09 +0100

    fix for bad merge

--- tests/DFE.FindInformationAcademiesTrusts.CypressTests/cypress/e2e/regression/trusts/trust-contacts-page.cy.ts
+++ tests/DFE.FindInformationAcademiesTrusts.CypressTests/cypress/e2e/regression/trusts/trust-contacts-page.cy.ts
@@ -11,53 +11,7 @@ function generateNameAndEmail() {
     };
 }
 
-
-// Helper function to check browser title for different contact pages
-function checkContactPageTitle(pageType: string) {
-    const titleMappings: Record<string, string> = {
-        'in-dfe': 'In DfE - Contacts - {trustName} - Find information about schools and trusts',
-        'in-the-trust': 'In this trust - Contacts - {trustName} - Find information about schools and trusts',
-        'edit-trm': 'Edit Trust relationship manager details - Contacts - {trustName} - Find information about schools and trusts',
-        'edit-sfso': 'Edit SFSO (Schools financial support and oversight) lead details - Contacts - {trustName} - Find information about schools and trusts'
-    };
-
-    commonPage
-        .checkThatBrowserTitleForTrustPageMatches(titleMappings[pageType]);
-}
-
-// Email validation test cases
-const emailValidationTestCases = [
-    {
-        description: "full non DFE email",
-        email: "email@hotmail.co.uk",
-        expectedErrors: ['Enter a DfE email address without any spaces']
-    },
-    {
-        description: "incorrect email format",
-        email: "email",
-        trmExpectedErrors: ['Enter an email address in the correct format, like name@education.gov.uk'],
-        sfsoExpectedErrors: ['Enter a DfE email address without any spaces', 'Enter an email address in the correct format, like name@education.gov.uk']
-    },
-    {
-        description: "illegal characters",
-        email: "@£$$^&",
-        trmExpectedErrors: ['Enter an email address in the correct format, like name@education.gov.uk'],
-        sfsoExpectedErrors: ['Enter a DfE email address without any spaces', 'Enter an email address in the correct format, like name@education.gov.uk']
-    },
-    {
-        description: "whitespace",
-        email: "a     b",
-        expectedErrors: ['Enter a DfE email address without any spaces', 'Enter an email address in the correct format, like name@education.gov.uk']
-    },
-    {
-        description: "email without prefix",
-        email: "@education.gov.uk",
-        expectedErrors: ['Enter an email address in the correct format, like name@education.gov.uk']
-    }
-];
-
 const incorrectEmailFormatMessage = 'Enter a DfE email address in the correct format, e.g. joe.bloggs@education.gov.uk';
-
 
 describe("Testing the components of the Trust contacts page", () => {
     testTrustData.forEach(({ typeOfTrust, uid }) => {
@@ -67,7 +21,8 @@ describe("Testing the components of the Trust contacts page", () => {
             });
 
             it("Checks the browser title is correct", () => {
-                checkContactPageTitle('in-dfe');
+                commonPage
+                    .checkThatBrowserTitleForTrustPageMatches('In DfE - Contacts - {trustName} - Find information about academies and trusts');
             });
 
             it("Checks the breadcrumb shows the correct page name", () => {
@@ -128,7 +83,8 @@ describe("Testing the components of the Trust contacts page", () => {
             });
 
             it("Checks the browser title is correct", () => {
-                checkContactPageTitle('edit-trm');
+                commonPage
+                    .checkThatBrowserTitleForTrustPageMatches('Edit Trust relationship manager details - Contacts - {trustName} - Find information about academies and trusts');
             });
         });
 
@@ -138,7 +94,8 @@ describe("Testing the components of the Trust contacts page", () => {
             });
 
             it("Checks the browser title is correct", () => {
-                checkContactPageTitle('edit-sfso');
+                commonPage
+                    .checkThatBrowserTitleForTrustPageMatches('Edit SFSO (Schools financial support and oversight) lead details - Contacts - {trustName} - Find information about academies and trusts');
             });
         });
 
@@ -148,7 +105,8 @@ describe("Testing the components of the Trust contacts page", () => {
             });
 
             it("Checks the browser title is correct", () => {
-                checkContactPageTitle('in-the-trust');
+                commonPage
+                    .checkThatBrowserTitleForTrustPageMatches('In this trust - Contacts - {trustName} - Find information about academies and trusts');
             });
 
             it("Checks the breadcrumb shows the correct page name", () => {
@@ -169,28 +127,6 @@ describe("Testing the components of the Trust contacts page", () => {
         beforeEach(() => {
             cy.visit('/trusts/contacts/in-dfe?uid=5527');
         });
-
-
-        describe('TRM email validation errors', () => {
-            emailValidationTestCases.forEach(({ description, email, expectedErrors, trmExpectedErrors }) => {
-                const errors = trmExpectedErrors ?? expectedErrors;
-                it(`Checks that ${description} entered returns the correct error message on a TRM`, () => {
-                    trustContactsPage.editTrustRelationshipManager("Name", email);
-                    errors.forEach(error => {
-                        commonPage.checkErrorPopup(error);
-                    });
-                });
-            });
-        });
-
-        describe('SFSO email validation errors', () => {
-            emailValidationTestCases.forEach(({ description, email, expectedErrors, sfsoExpectedErrors }) => {
-                const errors = sfsoExpectedErrors ?? expectedErrors;
-                it(`Checks that ${description} entered returns the correct error message on a SFSO`, () => {
-                    trustContactsPage.editSfsoLead("Name", email);
-                    errors.forEach(error => {
-                        commonPage.checkErrorPopup(error);
-                    });
 
         it("Checks that a full non DFE email entered returns the correct error message on a TRM ", () => {
             trustContactsPage
@@ -268,23 +204,22 @@ describe("Testing the components of the Trust contacts page", () => {
                         .editSfsoLead("Name", prefix);
                     commonPage
                         .checkErrorPopup(incorrectEmailFormatMessage);
-
                 });
             });
         });
     });
 
     describe("Testing the contacts sub navigation", () => {
-        const testUid = '5527';
 
         it('Should check that the contacts in dfe navigation button takes me to the correct page', () => {
-            cy.visit(`/trusts/contacts/in-the-trust?uid=${testUid}`);
+            cy.visit('/trusts/contacts/in-the-trust?uid=5527');
 
             trustContactsPage
                 .clickContactsInDfeSubnavButton()
                 .checkContactsInDfeSubHeaderPresent();
 
-            navigation.checkCurrentURLIsCorrect(`/trusts/contacts/in-dfe?uid=${testUid}`);
+            navigation
+                .checkCurrentURLIsCorrect('/trusts/contacts/in-dfe?uid=5527');
 
             trustContactsPage
                 .checkAllSubNavItemsPresent()
@@ -293,13 +228,14 @@ describe("Testing the components of the Trust contacts page", () => {
         });
 
         it('Should check that the contacts in this trust navigation button takes me to the correct page', () => {
-            cy.visit(`/trusts/contacts/in-dfe?uid=${testUid}`);
+            cy.visit('/trusts/contacts/in-dfe?uid=5527');
 
             trustContactsPage
                 .clickContactsInTheTrustSubnavButton()
                 .checkContactsInTheTrustSubHeaderPresent();
 
-            navigation.checkCurrentURLIsCorrect(`/trusts/contacts/in-the-trust?uid=${testUid}`);
+            navigation
+                .checkCurrentURLIsCorrect('/trusts/contacts/in-the-trust?uid=5527');
 
             trustContactsPage
                 .checkAllSubNavItemsPresent()
@@ -309,22 +245,18 @@ describe("Testing the components of the Trust contacts page", () => {
         });
 
         it('Should check that the contacts sub nav items are not present when I am not on the contacts page', () => {
-            cy.visit(`/trusts/overview/trust-details?uid=${testUid}`);
+            cy.visit('/trusts/overview/trust-details?uid=5527');
 
-            trustContactsPage.checkSubNavNotPresent();
+            trustContactsPage
+                .checkSubNavNotPresent();
         });
 
         describe("Testing a trust that has no contacts within it to ensure the issue of a 500 page appearing does not happen", () => {
-            const noContactsTrustUid = '17728';
-
             beforeEach(() => {
                 commonPage.interceptAndVerifyNo500Errors();
             });
 
-            [
-                `/trusts/contacts/in-dfe?uid=${noContactsTrustUid}`,
-                `/trusts/contacts/in-the-trust?uid=${noContactsTrustUid}`
-            ].forEach((url) => {
+            ['/trusts/contacts/in-dfe?uid=17728', '/trusts/contacts/in-the-trust?uid=17728'].forEach((url) => {
                 it(`Should have no 500 error on ${url}`, () => {
                     cy.visit(url);
                 });
