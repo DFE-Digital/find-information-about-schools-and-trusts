3c4b353999d8f1f385dc4f70ea0e6eef61ee68e4
Dan Livings <dan.livings@dxw.com>
2025-07-29 12:30:21 +0100

    Return short inspection data from `OfstedRepository.GetAcademiesInTrustOfstedAsync`
    This reuses the `OfstedShortInspection` record to get short inspection
    data for academies in a trust.
    
    This is done by reading
    `mis_mstr.Establishments_FIAT.date_of_latest_section_8_inspection` and
    `mis_mstr.Establishments_FIAT.section_8_inspection_overall_outcome` for
    the default case, and by reading
    `mis_mstr.FurtherEducationEstablishments_FIAT.date_of_latest_short_inspection`
    for those that can't be found in the `Establishments_FIAT` table. In
    this case, the outcome is set to `null` as there's no corresponding
    `short_inspection_overall_outcome` or similar column.

--- DfE.FindInformationAcademiesTrusts.Data/Repositories/Ofsted/AcademyOfsted.cs
+++ DfE.FindInformationAcademiesTrusts.Data/Repositories/Ofsted/AcademyOfsted.cs
@@ -4,6 +4,7 @@ public record AcademyOfsted(
     string Urn,
     string? EstablishmentName,
     DateTime DateAcademyJoinedTrust,
+    OfstedShortInspection ShortInspection,
     OfstedRating PreviousOfstedRating,
     OfstedRating CurrentOfstedRating
 );
