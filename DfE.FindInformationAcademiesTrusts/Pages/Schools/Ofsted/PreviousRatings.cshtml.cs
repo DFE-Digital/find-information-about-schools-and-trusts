a091e0ccb2b7cf8246c1ec66379f5b8dcbb3583c
Dan Livings <dan.livings@dxw.com>
2025-08-07 13:01:33 +0100

    Introduce an abstraction over the Ofsted pages to silence SonarQube

--- DfE.FindInformationAcademiesTrusts/Pages/Schools/Ofsted/PreviousRatings.cshtml.cs
+++ DfE.FindInformationAcademiesTrusts/Pages/Schools/Ofsted/PreviousRatings.cshtml.cs
@@ -1,34 +1,19 @@
 using DfE.FindInformationAcademiesTrusts.Data;
 using DfE.FindInformationAcademiesTrusts.Data.Enums;
 using DfE.FindInformationAcademiesTrusts.Pages.Shared;
+using DfE.FindInformationAcademiesTrusts.Services.Academy;
 using DfE.FindInformationAcademiesTrusts.Services.DataSource;
 using DfE.FindInformationAcademiesTrusts.Services.Export;
 using DfE.FindInformationAcademiesTrusts.Services.School;
 using DfE.FindInformationAcademiesTrusts.Services.Trust;
-using Microsoft.AspNetCore.Mvc;
 
 namespace DfE.FindInformationAcademiesTrusts.Pages.Schools.Ofsted;
 
-public class PreviousRatingsModel(ISchoolService schoolService, ISchoolOverviewDetailsService schoolOverviewDetailsService, ITrustService trustService, IDataSourceService dataSourceService, IOfstedSchoolDataExportService ofstedSchoolDataExportService, IDateTimeProvider dateTimeProvider, IOtherServicesLinkBuilder otherServicesLinkBuilder, ISchoolNavMenu schoolNavMenu) : OfstedAreaModel(schoolService, schoolOverviewDetailsService, trustService, dataSourceService, ofstedSchoolDataExportService, dateTimeProvider, otherServicesLinkBuilder, schoolNavMenu)
+public class PreviousRatingsModel(ISchoolService schoolService, ISchoolOverviewDetailsService schoolOverviewDetailsService, ITrustService trustService, IDataSourceService dataSourceService, IOfstedSchoolDataExportService ofstedSchoolDataExportService, IDateTimeProvider dateTimeProvider, IOtherServicesLinkBuilder otherServicesLinkBuilder, ISchoolNavMenu schoolNavMenu) : BaseRatingsModel(schoolService, schoolOverviewDetailsService, trustService, dataSourceService, ofstedSchoolDataExportService, dateTimeProvider, otherServicesLinkBuilder, schoolNavMenu)
 {
     public const string SubPageName = "Previous ratings";
     public override PageMetadata PageMetadata => base.PageMetadata with { SubPageName = SubPageName };
 
-    public OfstedRating OfstedRating { get; set; } = null!;
-    public BeforeOrAfterJoining InspectionBeforeOrAfterJoiningTrust { get; set; }
-
-    public override async Task<IActionResult> OnGetAsync()
-    {
-        var pageResult = await base.OnGetAsync();
-        
-        if (pageResult is NotFoundResult) return pageResult;
-        
-        var ofstedRatings = await SchoolService.GetSchoolOfstedRatingsAsync(Urn);
-
-        OfstedRating = ofstedRatings.PreviousOfstedRating;
-
-        InspectionBeforeOrAfterJoiningTrust = ofstedRatings.WhenDidPreviousInspectionHappen;
-        
-        return pageResult;
-    }
+    protected override OfstedRating GetOfstedRating(SchoolOfstedServiceModel ofstedRatings) => ofstedRatings.PreviousOfstedRating;
+    protected override BeforeOrAfterJoining GetWhenInspectionHappened(SchoolOfstedServiceModel ofstedRatings) => ofstedRatings.WhenDidPreviousInspectionHappen;
 }
