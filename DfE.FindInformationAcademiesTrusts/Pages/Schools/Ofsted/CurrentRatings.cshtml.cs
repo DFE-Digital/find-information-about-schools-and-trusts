949645731fd6832143445091ac285fdb3b3b4184
Dan Livings <dan.livings@dxw.com>
2025-08-07 12:09:56 +0100

    Add 'current ratings' page to the Ofsted area for a school or academy

---
+++ DfE.FindInformationAcademiesTrusts/Pages/Schools/Ofsted/CurrentRatings.cshtml.cs
@@ -0,0 +1,34 @@
+using DfE.FindInformationAcademiesTrusts.Data;
+using DfE.FindInformationAcademiesTrusts.Data.Enums;
+using DfE.FindInformationAcademiesTrusts.Pages.Shared;
+using DfE.FindInformationAcademiesTrusts.Services.DataSource;
+using DfE.FindInformationAcademiesTrusts.Services.Export;
+using DfE.FindInformationAcademiesTrusts.Services.School;
+using DfE.FindInformationAcademiesTrusts.Services.Trust;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DfE.FindInformationAcademiesTrusts.Pages.Schools.Ofsted;
+
+public class CurrentRatingsModel(ISchoolService schoolService, ISchoolOverviewDetailsService schoolOverviewDetailsService, ITrustService trustService, IDataSourceService dataSourceService, IOfstedSchoolDataExportService ofstedSchoolDataExportService, IDateTimeProvider dateTimeProvider, IOtherServicesLinkBuilder otherServicesLinkBuilder, ISchoolNavMenu schoolNavMenu) : OfstedAreaModel(schoolService, schoolOverviewDetailsService, trustService, dataSourceService, ofstedSchoolDataExportService, dateTimeProvider, otherServicesLinkBuilder, schoolNavMenu)
+{
+    public const string SubPageName = "Current ratings";
+    public override PageMetadata PageMetadata => base.PageMetadata with { SubPageName = SubPageName };
+
+    public OfstedRating OfstedRating { get; set; } = null!;
+    public BeforeOrAfterJoining InspectionBeforeOrAfterJoiningTrust { get; set; }
+
+    public override async Task<IActionResult> OnGetAsync()
+    {
+        var pageResult = await base.OnGetAsync();
+        
+        if (pageResult is NotFoundResult) return pageResult;
+        
+        var ofstedRatings = await SchoolService.GetSchoolOfstedRatingsAsync(Urn);
+
+        OfstedRating = ofstedRatings.CurrentOfstedRating;
+
+        InspectionBeforeOrAfterJoiningTrust = ofstedRatings.WhenDidCurrentInspectionHappen;
+        
+        return pageResult;
+    }
+}
