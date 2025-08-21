8bb3aefcce44eff1a89e635fbcff140b232d5fb9
Dan Livings <dan.livings@dxw.com>
2025-08-21 17:21:10 +0100

    Remove obsolete `TrustRepository.GetTrustGovernanceAsync` method
    This has now been fully replaced by the method in the
    `TrustGovernanceRepository`.

--- DfE.FindInformationAcademiesTrusts.Data/Repositories/Trust/ITrustRepository.cs
+++ DfE.FindInformationAcademiesTrusts.Data/Repositories/Trust/ITrustRepository.cs
@@ -4,7 +4,6 @@ public interface ITrustRepository
 {
     Task<TrustSummary?> GetTrustSummaryAsync(string uid);
     Task<TrustOverview> GetTrustOverviewAsync(string uid);
-    Task<TrustGovernance> GetTrustGovernanceAsync(string uid, string? urn = null);
     Task<TrustContacts> GetTrustContactsAsync(string uid, string? urn = null);
     Task<string> GetTrustReferenceNumberAsync(string uid);
 }
