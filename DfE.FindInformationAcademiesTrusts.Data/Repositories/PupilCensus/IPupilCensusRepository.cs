ead1cad26cf38113e79b36d68e6e05302e75169f
Dan Livings <dan.livings@dxw.com>
2025-09-12 11:40:55 +0100

    Add `PupilCensusRepository` method to get attendance statistics
    This introduces the `GetAttendanceStatisticsAsync` method, which returns
    the percentages of overall attendances (PERCTOT) and persistent absentees
    (PPERSABS10) from the spring census.

--- DfE.FindInformationAcademiesTrusts.Data/Repositories/PupilCensus/IPupilCensusRepository.cs
+++ DfE.FindInformationAcademiesTrusts.Data/Repositories/PupilCensus/IPupilCensusRepository.cs
@@ -3,4 +3,5 @@ namespace DfE.FindInformationAcademiesTrusts.Data.Repositories.PupilCensus;
 public interface IPupilCensusRepository
 {
     public Task<AnnualStatistics<SchoolPopulation>> GetSchoolPopulationStatisticsAsync(int urn);
+    public Task<AnnualStatistics<Attendance>> GetAttendanceStatisticsAsync(int urn);
 }
