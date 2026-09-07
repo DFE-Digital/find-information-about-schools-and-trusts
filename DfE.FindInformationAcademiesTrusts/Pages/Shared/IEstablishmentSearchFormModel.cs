d434240852eb678b2bf01583cee2860b77666279
DRYAN1 <dan.ryan@education.gov.uk>
2026-09-07 10:50:30 +0100

    search scss updated and search for a school page updated

---
+++ DfE.FindInformationAcademiesTrusts/Pages/Shared/IEstablishmentSearchFormModel.cs
@@ -0,0 +1,7 @@
+namespace DfE.FindInformationAcademiesTrusts.Pages.Shared;
+
+public interface IEstablishmentSearchFormModel
+{
+    string? KeyWords { get; set; }
+    string PageSearchFormInputId { get; }
+}
--- DfE.FindInformationAcademiesTrusts/Pages/WatchList/SearchForASchool.cshtml.cs
+++ DfE.FindInformationAcademiesTrusts/Pages/WatchList/SearchForASchool.cshtml.cs
@@ -3,10 +3,13 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace DfE.FindInformationAcademiesTrusts.Pages.WatchList;
 
-public class SearchForASchool : ContentPageModel
+public class SearchForASchool : ContentPageModel,IEstablishmentSearchFormModel
 {
+    
     public void OnGet()
     {
         
     }
+
+    public string PageSearchFormInputId => "school-search";
 }
